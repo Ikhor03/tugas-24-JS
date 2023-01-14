0d3154e6cd486b31d936ddbd05e5652e4e14386e
Ikhor03 <imamkhoiri725@gmail.com>
2023-01-14 15:35:30 +0700

    task 24

---
+++ tugas24.js
@@ -0,0 +1,8 @@
+let arr = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+console.log(`Sebeleumnya : ${arr}`);
+
+let arrSort = arr.sort();
+console.log(`Ascending : ${arrSort}`);
+
+let arrRev = arr.reverse();
+console.log(`Descending : ${arrRev}`);
